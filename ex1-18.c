a2329bf1a18c39ed5291ac8ee0b5ecc1c5ca44b7
b8h0v3n <78532406+b8h0v3n@users.noreply.github.com>
2026-06-16 02:47:37 +0200

    1.18 v2

--- ex1-18.c
+++ ex1-18.c
@@ -1,25 +1,49 @@
 #include<stdio.h>
 #define MAXLEN 1000
-#define IN 0
+#define IN 1
 #define OUT 0
-
-
-
-int main(void){
-  char line[MAXLEN];
-  return 0;
-}
 
 int my_getline(char arr[], int max){
   int len = 0;
-  while ((c = getchar()) != EOF && c != '\n'){
-    line[len] = c;
-    ++len;
+  int mode = OUT;
+  int c;
+  while((c = getchar()) != EOF && c != '\n'){
+    if((c != ' ' && c != '\t') && mode == OUT){
+      mode = IN;
+      arr[len] = c;
+      ++len;
+    }
+    else if((c != ' ' && c != '\t') && mode == IN){
+      arr[len] = c;
+      ++len;
+    }
+    else if((c == ' ' && c == '\t') && mode == IN){
+      mode = OUT;
+      arr[len] = c;
+      ++len;
+    }
+    else
+      ;
   }
-  return len;
+  switch (arr[len]){
+    case '\t':
+      arr[len] = '\n';
+    case ' ':
+      arr[len] = '\n';
+    default:
+      ++len;
+      arr[len] = '\0';
+  }
+ //returns the amount of characters in the string
+  return (len - 1);
 }
 
-int lineMod(char arr[]; int len):
-  int i = 0;
-  char e;
-  while (e = arr[i] =! '\n' && e != '\0')
+int main(void){
+  char line[MAXLEN];
+  int len;
+  while((len = my_getline(line, MAXLEN)) != 0){
+    printf("%s", line);
+    printf("%d", len);
+  }
+}
+
